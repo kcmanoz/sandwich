16f3d27bf887f912e617ee8ed4a3025b7aaed602
Manoj KC <manoj.kc@tuni.fi>
2023-02-14 22:29:27 +0200

    Navbar update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ const Navbar = () => {
 
       return (
         <div className='navbar'>
+          <span className='nav-title'>Sandwich House</span>
           <span className='order-btn'>Orders</span>
         </div>
       )
